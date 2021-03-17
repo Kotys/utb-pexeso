08b272a151e6e0326ac7d90f09e945083c7d6589
Kotys <kotrba@kotyslab.cz>
2021-03-17 17:08:53 +0100

    3 files modified

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ import './style.css';
 
 const appDiv = document.getElementById('my-app');
 
-
 const cards = [
   {
     icon: ''
   }
 ];
+
+const cardElements = appDiv.getElementsByClassName('card');
+for(const cardElement of cardElements) {
+  cardElement.addEventListener('click', toggleCardFn);
+}
+
+function toggleCardFn() {
+  this.classList.toggle('card-revealed');
+}
