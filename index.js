e47e19a319979fac0d49e22e861bfed4be485575
Kotys <kotrba@kotyslab.cz>
2021-03-17 15:41:32 +0100

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Import stylesheets
+import './reset.css';
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appDiv = document.getElementById('my-app');
+
+
+const cards = [
+  {
+    icon: ''
+  }
+];
