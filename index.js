f07a21d59305d607f7ef49520b2bb1736e57dbf4
Kotys <kotrba@kotyslab.cz>
2021-03-22 14:14:36 +0100

    1 file modified

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const pexeso = (containerId) => {
 	app.cardsMatched = [];
 
   /**
-	 * First revealed cards UUID
+	 * First revealed cards object
 	 */
   app.revealedCard = null;
 
@@ -34,14 +34,14 @@ const pexeso = (containerId) => {
     // TODO: Randomize
     cards.push({uuid: null, code: 'bell', icon: 'bell'});
 		cards.push({uuid: null, code: 'bug', icon: 'bug'});
-		cards.push({uuid: null, code: 'wifi', icon: 'wifi'});
-    cards.push({uuid: null, code: 'taxi', icon: 'taxi'});
-    cards.push({uuid: null, code: 'bicycle', icon: 'bicycle'});
-    cards.push({uuid: null, code: 'coffee', icon: 'coffee'});
-    cards.push({uuid: null, code: 'diamond', icon: 'diamond'});
-    cards.push({uuid: null, code: 'gift', icon: 'gift'});
-    cards.push({uuid: null, code: 'rocket', icon: 'rocket'});
-    cards.push({uuid: null, code: 'gamepad', icon: 'gamepad'});
+		// cards.push({uuid: null, code: 'wifi', icon: 'wifi'});
+    // cards.push({uuid: null, code: 'taxi', icon: 'taxi'});
+    // cards.push({uuid: null, code: 'bicycle', icon: 'bicycle'});
+    // cards.push({uuid: null, code: 'coffee', icon: 'coffee'});
+    // cards.push({uuid: null, code: 'diamond', icon: 'diamond'});
+    // cards.push({uuid: null, code: 'gift', icon: 'gift'});
+    // cards.push({uuid: null, code: 'rocket', icon: 'rocket'});
+    // cards.push({uuid: null, code: 'gamepad', icon: 'gamepad'});
 
     // Duplicate entries
     cards.push(...cards);
@@ -91,19 +91,44 @@ const pexeso = (containerId) => {
 		}
 	};
 
-  app.revealCardFn = () => {
-    return () => {
-
+  app.revealCardFnFactory = (card, cardElement) => {
+    return (event) => {
+      console.log(card);
+      cardElement.classList.add('card-revealed');
     };
   };
 
+  app.markCardAsRevealedFnFactory = (card) => {
+    // Check for already revealed card
+    if (app.revealedCard && app.revealedCard.uuid === card.uuid) {
+      return;
+    }
+    app.revealedCard = card;
 
-  app.toggle = () => {
-
+    console.log(app.revealedCard);
   };
 
-  app.initializeCard = (obj, cardFrontEl, cardBackendEl, cardEl) => {
+  app.checkMatch = (cardA, cardB) => {
+    return cardA.code === cardB.code;
+  };
 
+  app.initializeCard = (card, cardFrontEl, cardBackendEl, cardEl) => {
+    const revealCardFn = app.revealCardFnFactory(card, cardEl);
+    const markCardAsRevealedFn = app.markCardAsRevealedFnFactory(card);
+
+    const onClickFn = () => {
+      revealCardFn();
+      markCardAsRevealedFn();
+      if(app.revealedCard && app.revealedCard.uuid !== card.uuid) {
+        if(app.checkMatch(app.revealedCard, card)) {
+
+        } else {
+          // Hide them
+        }
+      }
+    };
+
+    cardFrontEl.addEventListener('click', onClickFn);
   };
 
 	return app;
